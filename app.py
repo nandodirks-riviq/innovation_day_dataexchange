abbb5c7e26a046606ca2dcb14be07916aea7867b
nandodirks-riviq <114667151+nandodirks-riviq@users.noreply.github.com>
2022-09-29 15:06:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask
-import pyodbc
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
 
 def connection():
     dbuser=os.environ['DBUSER'],
@@ -14,5 +10,25 @@ def connection():
     dbhost=os.environ['DBHOST'],
     dbname=os.environ['DBNAME']
     cstr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+s+';DATABASE='+d+';UID='+u+';PWD='+ p
-    conn = pyodbc.connect(cstr)
-    return conn
+    return cstr
+
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=connection(),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def testdb():
+    try:
+        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+        return '<h1>It works.</h1>'
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
+
+if __name__ == '__main__':
+    app.run(debug=True)
