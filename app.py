3ccd5b40c418cbe4edec3f4d72d712602c696eb3
nandodirks-riviq <114667151+nandodirks-riviq@users.noreply.github.com>
2022-09-29 14:55:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
+import pyodbc
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+def connection():
+    dbuser=os.environ['DBUSER'],
+    dbpass=os.environ['DBPASS'],
+    dbhost=os.environ['DBHOST'],
+    dbname=os.environ['DBNAME']
+    cstr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+s+';DATABASE='+d+';UID='+u+';PWD='+ p
+    conn = pyodbc.connect(cstr)
+    return conn
