30aaf6b24dcbfbcf8cdd98474f21befc73b209dc
nandodirks-riviq <114667151+nandodirks-riviq@users.noreply.github.com>
2022-09-30 10:02:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,16 +20,16 @@ app.config.update(
 
 db = SQLAlchemy(app)
 
-@app.route('/')
-def testdb():
-    try:
-        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
-        return f'<h1>It works.</h1>'
-    except Exception as e:
-        # e holds description of the error
-        error_text = "<p>The error:<br>" + str(e) + "</p>"
-        hed = '<h1>Something is broken.</h1>'
-        return hed + error_text
+# @app.route('/')
+# def testdb():
+#     try:
+#         db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+#         return f'<h1>It works.</h1>'
+#     except Exception as e:
+#         # e holds description of the error
+#         error_text = "<p>The error:<br>" + str(e) + "</p>"
+#         hed = '<h1>Something is broken.</h1>'
+#         return hed + error_text
     
 @app.route('/')
 def testquery():
