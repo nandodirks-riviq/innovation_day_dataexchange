97815b777445264204a349b33ea04fb837fdd7a9
nandodirks-riviq <114667151+nandodirks-riviq@users.noreply.github.com>
2022-10-03 12:44:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ completed_template = """
 def index():
     form = LanguageForm(request.form)
 
-    if request.method == 'POST' and form.validate():
+    if form.validate():
         print("POST request and form is valid")
         language =  form.getlist('language')
         print("languages in wsgi.py: %s" % request.form['language'])
