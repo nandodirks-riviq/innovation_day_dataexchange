5bcf93533066c9e299275bd1a1141ee9f749864e
nandodirks-riviq <114667151+nandodirks-riviq@users.noreply.github.com>
2022-10-03 10:29:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def testdb():
 @app.route('/query')
 def testdb2():
     try:
-        result = db.session.execute("SELECT * FROM INFORMATION_SCHEMA.columns WHERE TABLE_NAME = 'dbo.BuildVersion'")
+        result = db.session.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.columns WHERE TABLE_NAME = 'BuildVersion'")
         return f'<h1>{result.all()}</h1>'
     except Exception as e:
         return "<p>The error:<br>" + str(e) + "</p>"
